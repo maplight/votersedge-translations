0c4fa1834db8f544995330cc1d21734f51ba7079
LLTTDAY <day.elliott@gmail.com>
2016-03-11 10:38:33 -0800

    Updated contestsList.js

--- es/contestsList.js
+++ es/contestsList.js
@@ -3,9 +3,9 @@ import { canUseDOM } from "fbjs/lib/ExecutionEnvironment";
 // Applies to the list of contests that appears on the ballot overview, candidates tab, and the my list page.
 
 const contestsListTranslation = {
-  no_contests: 'There were no candidates found.', // Translate
-  bookmarked_contest: 'Review contest for', // Translate
-  party_label: 'Party', // Translate
+  no_contests: "No se encontraron candidatos.", // Translate
+  bookmarked_contest: "Revise la contienda para", // Translate
+  party_label: "Partido", // Translate
 };
 
 export default contestsListTranslation;
