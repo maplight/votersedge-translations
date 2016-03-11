b7e133edfc09e71bb62f8d86874b361cd9b73d6e
LLTTDAY <day.elliott@gmail.com>
2016-03-11 11:00:28 -0800

    Updated votingInfo.js

--- es/votingInfo.js
+++ es/votingInfo.js
@@ -3,14 +3,14 @@ import { canUseDOM } from "fbjs/lib/ExecutionEnvironment";
 // Applies to the voting info section
 
 const votingInfoTranslation = {
-    page_title: "Voting Info", // Translate
+    page_title: "Información de la votación", // Translate
     page_description: "",
-    website_label: "Website", // Translate
-    no_information_available: 'No information available', // Translate
-    check_my_polling_place: 'Check My Polling Place', // Translate
-    my_polling_place: 'My polling location and hours', // Translate
-    contact_state_election_office: 'Contact State Election Office', // Translate
-    contact_county_election_office: 'Contact County Election Office', // Translate
+    website_label: "Sitio web", // Translate
+    no_information_available: "No hay información disponible", // Translate
+    check_my_polling_place: "Verificar mi lugar de votación", // Translate
+    my_polling_place: "Ubicación de mi lugar de votación y horario", // Translate
+    contact_state_election_office: "Póngase en contacto con la Oficina de Elecciones Estatal", // Translate
+    contact_county_election_office: "Póngase en contacto con la Oficina de Elecciones del Condado", // Translate
 };
 
 export default votingInfoTranslation;
