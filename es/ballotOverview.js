70d5c24c36236fc6896175759eeec3d704e47a27
LLTTDAY <day.elliott@gmail.com>
2016-04-07 12:21:27 -0700

    Updated ballotOverview.js

--- es/ballotOverview.js
+++ es/ballotOverview.js
@@ -36,7 +36,7 @@ const ballotOverviewTranslation = {
     choose_voting_info: "Información de la votación", // Translate
     choose_voting_info_label: "Obtenga información sobre <strong>cuándo, dónde y cómo votar</strong>", // Translate
     choices: "Mi lista", // Translate
-    choices_label: "<p><strong>Mantenga un registro de sus opciones de boleta electoral y haga una lista para llevarla a las urnas.</p></strong><p>A medida que revise las competencias, los candidatos y las iniciativas de ley, puede añadirlos a su lista. Cuando termine, vaya a <strong>Mi lista</strong> para enviar la lista a su correo electrónico, impresora o una copia del enlace a su teléfono.</p><p>Somos un proyecto de defensa de los votantes no partidista y sin fines de lucro, y no le enviaremos nada ni haremos un seguimiento de sus opciones.</p>", // Translate
+    choices_label: "<p><strong>Mantenga un seguimiento de sus preferencias de boleta electoral y haga una lista para llevarla a las urnas.</p></strong><p>A medida que revise las competencias, los candidatos y las iniciativas de ley, puede añadirlos a su lista. Cuando termine, vaya a <strong>Mi lista</strong> para enviar la lista a su correo electrónico, impresora o una copia del enlace a su teléfono.</p><p>Somos un proyecto de defensa de los votantes no partidista y sin fines de lucro, y no le enviaremos nada ni haremos un seguimiento de sus opciones.</p>", // Translate
     noPollingLocationLabel: "Ubicación del lugar de votación y horario", // Translate
     noPollingLocationLookup: "<p><strong>¿Desea buscar su lugar de votación?</strong></p> <p></p>", // Translate
     noPollingLocationLinkLabel: "Buscar mi lugar de votación", // Translate
@@ -49,7 +49,7 @@ const ballotOverviewTranslation = {
       il: '6 a.m. - 7 p.m.',
       ny: '6 a.m. - 7 p.m.',
     },
-    more_voting_info: "Consulte más información sobre las votaciones", // Translate
+    more_voting_info: "Consulte más información sobre la votacion", // Translate
     other_address: "Busque la boleta electoral para", // Translate
     other_address_link: "otra dirección", // Translate
     other_elections: "Consulte su boleta electoral para", // Translate
