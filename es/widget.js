813023feacdb857f496de7e87a6c254c076fef38
Chacha Sikes <chachasikes@gmail.com>
2016-05-17 12:58:46 -0700

    fix spanish widget typo

--- es/widget.js
+++ es/widget.js
@@ -4,7 +4,7 @@ import { canUseDOM } from "fbjs/lib/ExecutionEnvironment";
 // This appears on all pages near the footer area and also on candidate, contest and measure pages.
 
 const widgetTranslation = {
-  find_ballot: 'Encuentrar su <br/><strong>boleta electoral</strong>',
+  find_ballot: 'Encontrar su <br/><strong>boleta electoral</strong>',
   street_address: 'Dirección',
   zipcode: 'Código Postal*',
   submit: 'Buscar',
