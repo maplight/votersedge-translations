f89376a878b32780f7e38e8494913e1e2643f8e4
LLTTDAY <day.elliott@gmail.com>
2016-10-14 16:58:32 -0700

    Update ballotOverview.js

--- es/ballotOverview.js
+++ es/ballotOverview.js
@@ -130,7 +130,7 @@ const ballotOverviewTranslation = {
     noPollingLocationLabel: "Ubicación del lugar de votación y horario", // Translate
     noPollingLocationLookup: "<p><strong>¿Desea buscar su lugar de votación?</strong></p> <p></p>", // Translate
     pollingLocationLookupToolLink: "Verifique la ubicación de su lugar de votación con la oficina electoral de su condado.", // Translate
-    noPollingLocationLookupToolLink: "There is no polling location look up available from your county elections office.", // Translate
+    noPollingLocationLookupToolLink: "No hay una búsqueda de la ubicación del lugar de votación disponible desde la oficina electoral de su condado.", // Translate
     contactCountyElectionsOffice: 'Póngase en contacto con la oficina electoral del condado',
     pollingLocationLabel: "La ubicación de su lugar de votación y horario", // Translate
     noPollingLocationMessage: 'No hay información disponible sobre la ubicación del lugar de votación.',
